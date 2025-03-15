8ff9184ba9a19cb64956dd18c278a9221e11ad4f
Dinorah Farias <dinorahfarias@outlook.com>
2025-03-15 11:50:37 -0300

    :sparkles: feat about me section

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.getElementById('toggle-info').addEventListener('click', function() {
+    var moreInfo = document.querySelector('.more-info');
+    var currentDisplay = moreInfo.style.display;
+    
+    if (currentDisplay === 'none' || currentDisplay === '') {
+        moreInfo.style.display = 'block';
+        this.textContent = '-';  
+    } else {
+        moreInfo.style.display = 'none';
+        this.textContent = '...';  
+    }
+});
